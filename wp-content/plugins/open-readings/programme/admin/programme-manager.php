88c3c6a2ba03f60baac4cd5ac1780edcd86be824
rimantasff <rimantas.naina@ff.stud.vu.lt>
2024-03-06 23:48:09 +0200

    updates

--- wp-content/plugins/open-readings/programme/admin/programme-manager.php
+++ wp-content/plugins/open-readings/programme/admin/programme-manager.php
@@ -8,6 +8,7 @@
 
     <h3>filter research areas: </h3>
     <form method="POST" id="filter">
+        <label>Session: </label>
         <select name="session_filter" id="session_filter_select">
             <option value="none">all</option>
             <?php
@@ -22,11 +23,12 @@
                     // Retrieve and display the custom field values
                     $display = get_post_meta(get_the_ID(), 'display_title', true);
                     if (get_post_meta(get_the_ID(), 'session_type', true) != -1)
-                        echo '<option value="' . get_the_ID() . '">' . $display . '</option>';
+                        echo '<option value="' . get_the_ID() . '"' . ((get_the_ID() == $_POST['session_filter']) ? 'selected' : '').'>' . $display . '</option>';
                 }}
             ?>
         </select>
 
+        <label>Reasearch: </label>
         <select name="ra_filter" id="ra_filter_select">
             <option value="none">all</option>
             <?php
@@ -41,11 +43,12 @@
             ?>
         </select>
 
+        <label>Type: </label>
         <select name="type_filter" id="type_filter_select">
             <option value="none">all</option>
             <?php
             global $PRESENTATION_TYPE;
-            for ($i = 1; $i < count($PRESENTATION_TYPE) + 1; $i++) {
+            for ($i = 1; $i < count($PRESENTATION_TYPE); $i++) {
                 if (isset($_POST['type_filter']) && $_POST['type_filter'] == $i) {
                     echo '<option value="' . $i . '" selected>' . array_search($i, $PRESENTATION_TYPE) . '</option>';
                 } else
@@ -54,10 +57,27 @@
 
             ?>
         </select>
+        <label>Assigned: </label>
+        <select name="assigned_filter" id="assigned_filter_select">
+            <option value="none">all</option>
+            <?php
+            $status = [
+                1 => 'unassigned',
+                2 => 'assigned'
+            ];
+            for ($i = 1; $i < count($status)+1; $i++) {
+                if (isset($_POST['assigned_filter']) && $_POST['assigned_filter'] == $i) {
+                    echo '<option value="' . $i . '" selected>' . $status[$i] . '</option>';
+                } else
+                    echo '<option value="' . $i . '">' . $status[$i] . '</option>';
+            }
+
+            ?>
+        </select>
 
 </div>
 <form method=post>
-<button name="save_settings" type="submit">Save All decisions</button>
+<button name="save_settings" type="submit">Save All</button>
 <div>
     <table cellspacing=0 cellpadding=1 border=1 bordercolor=white width=100%>
         <tr>
@@ -88,28 +108,60 @@
                     $sql = $wpdb->prepare("SELECT * FROM $joint_table WHERE hash_id = %s", $id);
                     $result = $wpdb->get_row($sql);
 
-                    $presentation_data = array(
-                        'post_title'    => $result->first_name . ' ' . $result->last_name,
-                        'post_content'  => '',
-                        'post_status'   => 'publish',
-                        'post_type'     => 'presentation',
-                        'meta_input'    => array(
-                            'first_name' => $result->first_name,
-                            'last_name' => $result->last_name,
-                            'research_area' => $result->research_area,
-                            'presentation_title' => $result->title,
-                            'abstract_pdf' => $result->pdf,
-                            'presentation_type' => $result->decision,
-                            'hash_id' => $id,
-                            'presentation_session' => $_POST['session-name'][$id],
-                            'presentation_start' => $_POST['session-start'][$id],
-                            'poster_number' => $_POST['session-poster'][$id],
+                    if(!isset($_POST['field-id'][$id])){
 
-                        )
-                    );
-                    
-                    // Insert the post into the database
-                    $post_id = wp_insert_post($presentation_data);
+                        $presentation_data = array(
+                            'post_title'    => $result->first_name . ' ' . $result->last_name,
+                            'post_content'  => '',
+                            'post_status'   => 'publish',
+                            'post_type'     => 'presentation',
+                            'meta_input'    => array(
+                                'first_name' => $result->first_name,
+                                'last_name' => $result->last_name,
+                                'research_area' => $result->research_area,
+                                'presentation_title' => $result->display_title,
+                                'abstract_pdf' => $result->pdf,
+                                'presentation_type' => $result->decision,
+                                'hash_id' => $id,
+                                'presentation_session' => $_POST['session-name'][$id],
+                                'presentation_start' => $_POST['session-start'][$id],
+                                'presentation_end' => $_POST['session-end'][$id],
+                                'poster_number' => $_POST['session-poster'][$id],
+
+                            )
+                        );
+                        
+                        // Insert the post into the database
+                        $post_id = wp_insert_post($presentation_data);
+                    } else {
+                        $update_presentation_data = array(
+                            'ID'            => $_POST['field-id'][$id],
+                            'post_title'    => $result->first_name . ' ' . $result->last_name,
+                            'post_content'  => '',
+                            'post_status'   => 'publish',
+                            'post_type'     => 'presentation',
+                            'meta_input'    => array(
+                                'first_name' => $result->first_name,
+                                'last_name' => $result->last_name,
+                                'research_area' => $result->research_area,
+                                'presentation_title' => $result->display_title,
+                                'abstract_pdf' => $result->pdf,
+                                'presentation_type' => $result->decision,
+                                'hash_id' => $id,
+                                'presentation_session' => $_POST['session-name'][$id],
+                                'presentation_start' => $_POST['session-start'][$id],
+                                'presentation_end' => $_POST['session-end'][$id],
+                                'poster_number' => $_POST['session-poster'][$id],
+
+                            )
+                        );
+                        
+                        $post_id = wp_update_post($update_presentation_data);
+                    }
+
+                    if(isset($_POST['delete'][$id])){
+                        $delete = wp_delete_post($_POST['field-id'][$id], true);
+                    }
                 }
             }
         }
@@ -123,15 +175,38 @@
 
         global $STATUS_CODES;
         $query = "SELECT * FROM $joint_table WHERE (decision = 1 OR decision = 2)";
+        
 
 
         if ($ra_filter != 'none') {
             $query .= " AND research_area='$RESEARCH_AREAS[$ra_filter]'";
+            $query_array[] = array(
+                'key' => 'research_area',
+                'value' => $RESEARCH_AREAS[$ra_filter],
+                'compare' => '=',
+            );
         }
         if (isset($_POST['type_filter'])) {
             $type_filter = $_POST['type_filter'];
             if ($type_filter != 'none') {
                 $query .= " AND decision=$type_filter";
+                $query_array[] = array(
+                    'key' => 'presentation_type',
+                    'value' => $type_filter,
+                    'compare' => '=',
+                );
+            }
+        }
+
+        if (isset($_POST['session_filter'])) {
+            $session_filter = $_POST['session_filter'];
+            if ($session_filter != 'none') {
+                // $query .= " AND decision=$type_filter";
+                $query_array[] = array(
+                    'key' => 'presentation_session',
+                    'value' => $session_filter,
+                    'compare' => '=',
+                );
             }
         }
 
@@ -151,21 +226,28 @@
         $results = $wpdb->get_results($query);
 
 
+
         $presentation_posts = new WP_Query(array(
             'post_type' => 'presentation', // Replace with your custom post type
             'posts_per_page' => -1,
+            'meta_query' => array(
+                'relation' => 'AND',
+                $query_array
+            )
         ));
         
-        while ($presentation_posts->have_posts()) {
-            $id = get_post_meta(get_the_ID(), 'hash_id', true);
+        if($_POST['assigned_filter'] != '1') while ($presentation_posts->have_posts()) {
             $presentation_posts->the_post();
-            echo '<tr style="background-color:;">';
-            echo "<td>" . get_post_meta(get_the_ID(), 'first_name', true) . "</td>";
-            echo "<td>" . get_post_meta(get_the_ID(), 'last_name', true) . "</td>";
-            echo "<td>" . get_post_meta(get_the_ID(), 'presentation_title', true) . "</td>";
-            echo "<td>" . get_post_meta(get_the_ID(), 'research_area', true) . "</td>";
-            echo "<td> <a href=\"". get_post_meta(get_the_ID(), 'abstract_pdf', true) . "?timestamp=" . time() . "\">" . basename(get_post_meta(get_the_ID(), 'abstract_pdf', true)) . "</a></td>";
-            echo '<td><strong>' . array_search(get_post_meta(get_the_ID(), 'presentation_type', true), $PRESENTATION_TYPE) . '</strong> <br>';
+            $post_id = get_the_ID();
+            $id = get_post_meta($post_id, 'hash_id', true);
+            $presentation_session = get_post_meta($post_id, 'presentation_session', true);
+            echo '<tr style="background-color:#ffa;">';
+            echo "<td>" . get_post_meta($post_id, 'first_name', true) . "</td>";
+            echo "<td>" . get_post_meta($post_id, 'last_name', true) . "</td>";
+            echo "<td>" . get_post_meta($post_id, 'presentation_title', true) . "</td>";
+            echo "<td>" . get_post_meta($post_id, 'research_area', true) . "</td>";
+            echo "<td> <a href=\"". get_post_meta($post_id, 'abstract_pdf', true) . "?timestamp=" . time() . "\">" . basename(get_post_meta($post_id, 'abstract_pdf', true)) . "</a></td>";
+            echo '<td><strong>' . array_search(get_post_meta($post_id, 'presentation_type', true), $PRESENTATION_TYPE) . '</strong> <br>';
             echo "</td>";
             echo '<td><select name="session-name[' . $id . ']">';
             echo '<option value="none">Select session</option>';
@@ -179,28 +261,33 @@
                     $session_query->the_post();
                     // Retrieve and display the custom field values
                     $display = get_post_meta(get_the_ID(), 'display_title', true);
-                    if (get_post_meta(get_the_ID(), 'session_type', true) != -1)
-                        echo '<option value="' . get_the_ID() . '">' . $display . '</option>';
+                    if (get_post_meta(get_the_ID(), 'session_type', true) != -1){
+                        if (get_the_ID() == $presentation_session){
+                            echo '<option value="' . get_the_ID() . '" selected>' . $display . '</option>';
+                        } else {
+                            echo '<option value="' . get_the_ID() . '">' . $display . '</option>';
+                        }
+                    }
                 }
             }
             echo '</select></td>';
-            echo '<td><label>Start: </label><input type="time" name="session-start[' . $id . ']"><br>
-            <label>End: </label><input type="time" name="session-end[' . $id . ']"></td>';
-            echo '<td><input type="number" name="session-poster[' . $id . ']"></td>';
-            echo '<td><input type="checkbox"></td>';
-            echo "</tr></form>";
+            echo '<td><label>Start: </label><input type="time" name="session-start[' . $id . ']" value="' . get_post_meta($post_id, 'presentation_start', true) . '"><br>
+            <label>End: </label><input type="time" name="session-end[' . $id . ']" value="' . get_post_meta($post_id, 'presentation_end', true) . '"></td>';
+            echo '<td><input type="number" name="session-poster[' . $id . ']" value="' . get_post_meta($post_id, 'poster_number', true) . '"></td>';
+            echo '<td><input type="checkbox" name="delete[' . $id . ']"> <input type="text" name="field-id[' . $id . ']" value="'. $post_id .'" hidden></td>';
+            echo "</tr>";
         }
         wp_reset_postdata();
 
 
 
-        foreach ($results as $result) {
+        if($_POST['assigned_filter'] != '2') foreach ($results as $result) {
             $id = $result->hash_id;
             if (!in_array($id, $presentation_post_ids)) {
                 $first_name = $result->first_name;
                 $last_name = $result->last_name;
                 // $affiliation = $result->affiliation;
-                $presentation_title = $result->title;
+                $presentation_title = $result->display_title;
                 $abstract_pdf = $result->pdf;
                 $research_area = $result->research_area;
                 $presentation_type = "";
@@ -235,7 +322,7 @@
                 <label>End: </label><input type="time" name="session-end[' . $id . ']"></td>';
                 echo '<td><input type="number" name="session-poster[' . $id . ']"></td>';
                 echo '<td><input type="checkbox"></td>';
-                echo "</tr></form>";
+                echo "</tr>";
 
         }
     }
@@ -243,7 +330,7 @@
 
         ?>
 
-    </table>
+</table></form>
 
 
 </div>
@@ -257,6 +344,12 @@
             $('#filter').submit();
         });
         $('#type_filter_select').change(function () {
+            $('#filter').submit();
+        });
+        $('#session_filter_select').change(function () {
+            $('#filter').submit();
+        });
+        $('#assigned_filter_select').change(function () {
             $('#filter').submit();
         });
     });
