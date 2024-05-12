e2d6ccc95bc0a48521d483be044ebec9ce11ece2
MartynasKer <martynaskersys@gmail.com>
2024-05-12 20:49:02 +0300

    research area and session title in abstract generation

--- wp-content/plugins/open-readings/programme/generate-abstract.php
+++ wp-content/plugins/open-readings/programme/generate-abstract.php
@@ -1,11 +1,12 @@
 <?php
 
 
-function download_abstract(){
-    if (!isset($_GET['page']) or $_GET['page'] != 'or_programme_abstract') 
+function download_abstract()
+{
+    if (!isset($_GET['page']) or $_GET['page'] != 'or_programme_abstract')
         return;
 
-    if(!isset($_POST['download-abstracts']))
+    if (!isset($_POST['download-abstracts']))
         return;
     $abstract_fields = get_abstract_data();
     // var_dump($abstract_fields);
@@ -13,17 +14,17 @@ function download_abstract(){
     global $wpdb;
     $results = $wpdb->get_results("SELECT person_hash_id FROM wp_or_registration_presentations");
     $all_hash_ids = array();
-    foreach ($results as $result){
+    foreach ($results as $result) {
         $all_hash_ids[] = $result->person_hash_id;
     }
 
-    if(isset($_POST['abstract-hash-ids']) and trim($_POST['abstract-hash-ids']) != ''){
+    if (isset($_POST['abstract-hash-ids']) and trim($_POST['abstract-hash-ids']) != '') {
         $hash_ids = explode("\n", $_POST['abstract-hash-ids']);
-        foreach($hash_ids as $key => $id){
+        foreach ($hash_ids as $key => $id) {
             $hash_ids[$key] = trim($id);
         }
-        foreach($hash_ids as $key => $id){
-            if(!in_array($id, $all_hash_ids))
+        foreach ($hash_ids as $key => $id) {
+            if (!in_array($id, $all_hash_ids))
                 unset($hash_ids[$key]);
         }
     } else {
@@ -45,22 +46,22 @@ function download_abstract(){
     }
 
     $zip->addFile($dataFile, $dataFile);
-    if(isset($_POST['download-figures'])){
+    if (isset($_POST['download-figures'])) {
         $zip->addEmptyDir('figures');
         $zip->addFile(__DIR__ . '/orstylet.sty', 'orstylet.sty');
-        
-        foreach($hash_ids as $id){
+
+        foreach ($hash_ids as $id) {
             $folder = $abstract_fields[$id]['session_id'];
             $images = $abstract_fields[$id]['images'];
             $path = ABSPATH . 'wp-content/latex/' . $folder;
-            foreach($images as $image){
+            foreach ($images as $image) {
                 $file_path = $path . '/images/' . $image;
                 $zip->addFile($file_path, "figures/{$folder}/{$image}");
             }
         }
     }
-    
-    
+
+
     $zip->close();
     // Set headers to initiate download
     header('Content-disposition: attachment; filename=' . $zip_file);
@@ -72,11 +73,12 @@ function download_abstract(){
 
 }
 
-function generate_abstract_book($hash_ids){
+function generate_abstract_book($hash_ids)
+{
     $abstract_fields = get_abstract_data();
     $content = wp_unslash(get_option('abstract-book-preamble'));
 
-    foreach($hash_ids as $hash){
+    foreach ($hash_ids as $hash) {
         $content .= get_abstract_page($hash, $abstract_fields);
     }
 
@@ -87,8 +89,9 @@ function generate_abstract_book($hash_ids){
     return $content;
 }
 
-function get_abstract_page($hash_id, $abstract_fields){
-    $title = $abstract_fields[$hash_id]['display_title'];
+function get_abstract_page($hash_id, $abstract_fields)
+{
+    $title = $abstract_fields[$hash_id]['title'];
     $title = abstract_format_title($title);
 
     $i = 1;
@@ -101,13 +104,13 @@ function get_abstract_page($hash_id, $abstract_fields){
         $aff_ref = $auth[1];
         //replace everything that is not a digit or ,
 
-        if (isset($auth[2])){
+        if (isset($auth[2])) {
             $authors = $authors . '\underline{' . $name . '}$^{' . $aff_ref . '}$';
             $author_email = $auth[2];
         } else
             $authors = $authors . $name . '$^{' . $aff_ref . '}$';
 
-        if ($i < count($authors_array)){
+        if ($i < count($authors_array)) {
             $authors = $authors . ', ';
             $author_list = $author_list . ', ';
         }
@@ -127,7 +130,7 @@ function get_abstract_page($hash_id, $abstract_fields){
 
     $reference_array = $abstract_fields[$hash_id]['references'];
 
-    if(is_array($reference_array) and count($reference_array) > 0){
+    if (is_array($reference_array) and count($reference_array) > 0) {
         // var_dump($reference_array);
         $references = '
     \vfill    
@@ -135,17 +138,17 @@ function get_abstract_page($hash_id, $abstract_fields){
         ';
         $i = 1;
         foreach ($reference_array as $ref) {
-           $references .= '\bibitem{' . $i . '} ' . $ref . '
+            $references .= '\bibitem{' . $i . '} ' . $ref . '
            ';
-           $i++;
+            $i++;
         }
         $references .= '
         \end{thebibliography}
         ';
-       
-        } else{
-            $references = '';
-        }
+
+    } else {
+        $references = '';
+    }
 
     $abstractContent = $abstract_fields[$hash_id]['content'];
 
@@ -166,7 +169,9 @@ function get_abstract_page($hash_id, $abstract_fields){
         '${affiliations}' => $affiliations,
         '${content}' => $abstractContent,
         '${references}' => $references,
-        '${author_list}' => $author_list
+        '${author_list}' => $author_list,
+        '${session}' => $abstract_fields[$hash_id]['session'],
+        '${research_area}' => $abstract_fields[$hash_id]['research_area']
 
         // Add more placeholders and values as needed
     );
@@ -178,25 +183,57 @@ function get_abstract_page($hash_id, $abstract_fields){
     return $templateContent;
 }
 
-function get_abstract_data(){
+function get_abstract_data()
+{
     global $wpdb;
 
-    $results = $wpdb->get_results("SELECT person_hash_id, display_title, authors, affiliations, `references`, content, images, session_id FROM wp_or_registration_presentations");
-   
+    $results = $wpdb->get_results("SELECT person_hash_id, title, authors, affiliations, `references`, content, images, session_id FROM wp_or_registration_presentations");
+
+
+
     $abstract_fields = array();
-    foreach($results as $result){
-        $abstract_fields[$result->person_hash_id]['display_title'] = $result->display_title;
+    foreach ($results as $result) {
+        $abstract_fields[$result->person_hash_id]['title'] = $result->title;
         $abstract_fields[$result->person_hash_id]['authors'] = json_decode($result->authors);
         $abstract_fields[$result->person_hash_id]['affiliations'] = json_decode($result->affiliations);
         $abstract_fields[$result->person_hash_id]['references'] = wp_unslash(json_decode($result->references));
         $abstract_fields[$result->person_hash_id]['content'] = wp_unslash($result->content);
         $abstract_fields[$result->person_hash_id]['images'] = json_decode($result->images, ARRAY_A);
         $abstract_fields[$result->person_hash_id]['session_id'] = $result->session_id;
+
+        $research_area = $wpdb->get_results("SELECT research_area FROM wp_or_registration WHERE hash_id = '$result->person_hash_id'");
+        $abstract_fields[$result->person_hash_id]['research_area'] = $research_area[0]->research_area;
+
+
+        $presenter_args = array(
+            'post_type' => 'presentation',
+            'meta_query' => array(
+                array(
+                    'key' => 'hash_id',
+                    'value' => $result->person_hash_id
+                )
+            )
+        );
+
+        $presenter_query = new WP_Query($presenter_args);
+        $presenter = $presenter_query->posts[0];
+        $session = get_field('session', $presenter->ID);
+        $session_args = array(
+            'post_type' => 'session',
+            'p' => $session->ID
+        );
+        $session_query = new WP_Query($session_args);
+        $short_title = get_field('short_title', $session_query->posts[0]->ID);
+
+        $abstract_fields[$result->person_hash_id]['session'] = $short_title;
+
+
     }
     return $abstract_fields;
 }
 
-function abstract_format_title($title){
+function abstract_format_title($title)
+{
     $title = fixUnclosedTags($title, '<sup>', '</sup>');
 
     // Add missing </sub> tags
@@ -262,11 +299,11 @@ function abstract_format_title($title){
 
     }
 
-    
-    
+
+
     $title = str_replace('&nbsp;', ' ', $title);
 
     return $title;
 
-    
+
 }
