9c569712b3b1cf55b7e4753e2d18b86767bea4e5
Rimantas Naina <rimantas.naina@gmail.com>
2024-12-01 16:03:18 +0200

    style and script registration moved outside open-readings.php file

---
+++ wp-content/plugins/open-readings/include/register-styles.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+function register_styles(){
+
+    $styles = [
+        'faq-widget-style' => 'assets/css/faq-widget-style.css',
+        'highlight-style' => 'assets/css/github.css',
+        'registration-widget-style' => 'assets/css/registration-widget-style.css',
+        'programme-day-style' => 'assets/css/programme-day-style.css',
+        'latex-field-style' => 'assets/css/latex-field-style.css',
+        'registration-evaluation-style' => 'assets/css/evaluation-style.css',
+    ];
+
+    foreach ($styles as $handle => $path) {
+        $css_path = plugin_dir_path(__FILE__) . $path;
+        wp_register_style($handle, plugins_url($path, OR_PLUGIN_FILE), array(), filemtime($css_path));
+    }
+}
+
+add_action('wp_enqueue_scripts', 'register_styles');
--- wp-content/plugins/open-readings/open-readings.php
+++ wp-content/plugins/open-readings/open-readings.php
@@ -23,6 +23,8 @@ use OpenReadings\Registration\Registration_Session\ORRegistrationSession;
  * Update URI:        https://example.com/my-plugin/
  */
 
+define('OR_PLUGIN_DIR', __DIR__ . '/');
+define('OR_PLUGIN_FILE', __FILE__);
 function register_faq_widget($widgets_manager)
 {
   require_once (__DIR__ . '/widgets/faq-widget.php');
@@ -31,6 +33,8 @@ function register_faq_widget($widgets_manager)
   $widgets_manager->register(new \Elementor_Assigned_Session_Widget());
 }
 
+require_once __DIR__ . '/include/register-styles.php';
+require_once __DIR__ . '/include/register-scripts.php';
 
 function register_programme_day_widget($widgets_manager)
 {
@@ -39,47 +43,10 @@ function register_programme_day_widget($widgets_manager)
 }
 
 add_action('elementor/widgets/register', 'register_programme_day_widget');
-
 add_action('elementor/widgets/register', 'register_faq_widget');
 
 function register_or_dependencies()
 {
-  $version = '1';
-  $css_path = plugin_dir_path(__FILE__) . 'assets/css/faq-widget-style.css';
-  wp_register_style('faq-widget-style', plugins_url('assets/css/faq-widget-style.css', __FILE__), array(), filemtime($css_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/faq-widget-js.js';
-  wp_register_script('faq-widget-js', plugins_url('assets/js/faq-widget-js.js', __FILE__), array(), filemtime($js_path));
-  wp_register_style('highlight-style', plugins_url('assets/css/github.css', __FILE__));
-  wp_register_style('registration-widget-style', plugins_url('assets/css/registration-widget-style.css', __FILE__));
-
-  $css_path = plugin_dir_path(__FILE__) . 'assets/css/programme-day-style.css';
-  wp_register_style('programme-day-style', plugins_url('assets/css/programme-day-style.css', __FILE__), array(), filemtime($css_path));
-  $css_path = plugin_dir_path(__FILE__) . 'assets/css/latex-field-style.css';
-  wp_register_style('latex-field-style', plugins_url('assets/css/latex-field-style.css', __FILE__), array(), filemtime($css_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/program-day-js.js';
-  wp_register_script('programme-day-js', plugins_url('assets/js/program-day-js.js', __FILE__), array(), filemtime($js_path));
-
-
-  wp_register_script('highlight-js', plugins_url('assets/js/highlight.js', __FILE__));
-  wp_register_script('latex-min-js', plugins_url('assets/js/latex.min.js', __FILE__));
-  wp_register_script('country-field-js', plugins_url('assets/js/country-field-js.js', __FILE__));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/institution-field-js.js';
-  wp_register_script('institution-field-js', plugins_url('assets/js/institution-field-js.js', __FILE__), array(), filemtime($js_path));
-  wp_register_script('institutions-list-js', plugins_url('assets/js/institutions-list-js.js', __FILE__));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/latex-field-js.js';
-  wp_register_script('latex-field-js', plugins_url('assets/js/latex-field-js.js', __FILE__), array(), filemtime($js_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/authors-field-js.js';
-  wp_register_script('authors-field-js', plugins_url('assets/js/authors-field-js.js', __FILE__), array(), filemtime($js_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/affiliation-field-js.js';
-  wp_register_script('affiliation-field-js', plugins_url('assets/js/affiliation-field-js.js', __FILE__), array(), filemtime($js_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/reference-field-js.js';
-  wp_register_script('reference-field-js', plugins_url('assets/js/reference-field-js.js', __FILE__), array(), filemtime($js_path));
-  $js_path = plugin_dir_path(__FILE__) . 'assets/js/image-field-js.js';
-  wp_register_script('image-field-js', plugins_url('assets/js/image-field-js.js', __FILE__), array(), filemtime($js_path));
-  wp_register_script('title-field-js', plugins_url('assets/js/title-field-js.js', __FILE__));
-  wp_register_script('jquery-js', plugins_url('assets/js/jquery-3.6.4.min.js', __FILE__));
-
-
   $data_to_pass = array(
     'path' => content_url(),
     // Use admin-ajax.php for AJAX requests
@@ -90,16 +57,6 @@ function register_or_dependencies()
 }
 
 add_action('wp_enqueue_scripts', 'register_or_dependencies');
-
-function register_evaluation_styles()
-{
-  wp_register_style('registration-evaluation-style', plugins_url('assets/css/evaluation-style.css', __FILE__));
-  wp_register_script('institutions-list-js', plugins_url('assets/js/evaluation-js.js', __FILE__));
-  wp_register_script('evaluation-js', plugins_url('assets/js/evaluation-js.js', __FILE__));
-
-}
-
-add_action('admin_enqueue_scripts', 'register_evaluation_styles');
 
 
 function register_faq_controls($controls_manager)
@@ -151,7 +108,6 @@ add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
 function add_menu_roles()
 {
 
-
   //add new capability
   $role = get_role('administrator');
   $role->add_cap('manage_or_registration');
@@ -184,8 +140,6 @@ function add_menu_roles()
       'manage_evaluations' => false,
     )
   );
-
-
 }
 
 
@@ -206,25 +160,16 @@ function register_admin()
 add_action('init', 'register_admin');
 add_action('init', 'register_or_mailer');
 
-
-
-define('OR_PLUGIN_DIR', __DIR__ . '/');
-
-
 function add_new_form_actions($form_actions_registrar)
 {
 
   require_once (__DIR__ . '/form-actions/or-form-action.php');
   require_once (__DIR__ . '/form-actions/or-presentation-redirect.php');
   require_once (__DIR__ . '/form-actions/custom-form.php');
-  //require_once(__DIR__ . '/form-actions/or-update-form.php');
+
   $form_actions_registrar->register(new \Custom_Elementor_Form_Action());
   $form_actions_registrar->register(new \ORPresentationUpload());
-  //$form_actions_registrar->register(new \ORUpdateFormAction());
   $form_actions_registrar->register(new \ORMainRegistrationSubmit());
-
-
-
 
 }
 add_action('elementor_pro/forms/actions/register', 'add_new_form_actions');
@@ -242,11 +187,6 @@ function add_new_form_field($form_fields_registrar)
   require_once (__DIR__ . '/form-fields/title-field.php');
   require_once (__DIR__ . '/form-fields/simple-check-field.php');
 
-
-
-
-
-
   $form_fields_registrar->register(new \Elementor_Country_Field());
   $form_fields_registrar->register(new \Elementor_Institution_Field());
   $form_fields_registrar->register(new \Elementor_Latex_Field());
@@ -256,8 +196,6 @@ function add_new_form_field($form_fields_registrar)
   $form_fields_registrar->register(new \Elementor_Image_Field());
   $form_fields_registrar->register(new \TitleField());
   $form_fields_registrar->register(new \Elementor_Simple_Check_Field());
-
-
 
 }
 add_action('elementor_pro/forms/fields/register', 'add_new_form_field');
@@ -270,31 +208,15 @@ add_action('elementor_pro/forms/fields/register', 'add_new_form_field');
 
 // }
 
-
-
 // add_action('init', 'populate_registration_form');
 
 function register_or_mailer_admin()
 {
   require_once (__DIR__ . '/mailer/admin.php');
   $admin = new ORmailerAdmin();
-
-
 }
+
 add_action('init', 'register_or_mailer_admin');
-
-
-// function enqueue_form_fill_script()
-// {
-//   if (did_action('wp_loaded') > 1) {
-//     return;
-//   }
-//   if (strpos($_SERVER['REQUEST_URI'], 'registration') !== false) {
-//     require_once (__DIR__ . '/registration/begin-session.php');
-//   }
-// }
-
-// add_action('wp_loaded', 'enqueue_form_fill_script');
 
 function my_custom_function()
 {
@@ -359,8 +281,6 @@ add_filter('rest_presentation_query', function ($args, $request) {
     );
     $args['per_page'] = 200; // Set your desired maximum limit
     // remove page limit
-
-
   }
 
 
@@ -391,8 +311,6 @@ function or_registration_cookies() {
       global $or_session;
       require_once __DIR__ . '/registration/registration-session.php';
       $or_session = new ORRegistrationSession();
-
   }
 }
-// Hook the function to 'template_redirect' or 'wp'
 add_action('template_redirect', 'or_registration_cookies');
