f3034cd7053641e6a9dcef93847fb21a512c916a
Rimantas Naina <rimantas.naina@gmail.com>
2026-02-09 20:02:25 +0200

    change email date

--- wp-content/plugins/open-readings/evaluation/admin/registration-functions.php
+++ wp-content/plugins/open-readings/evaluation/admin/registration-functions.php
@@ -470,7 +470,7 @@ function send_update()
             ' . $_SESSION['e_hash'] . '<br>
             To update your submission please click <strong><a href="https://openreadings.eu/registration?id=' . $_SESSION['e_hash'] . '">HERE</a></strong><br><br>
 
-            Note: In order to be accepted into the conference, you must make these changes until March 3rd <br>
+            Note: In order to be accepted into the conference, you must make these changes until February 23rd <br>
             If you have further questions, please reach out to <a href="mailto:info@openreadings.eu">info@openreadings.eu</a><br><br>
 
             Best regards, <br>
